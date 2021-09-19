4f00e2e6f3f3f8fd07bef8b583c3c9ea144dac75
gagikpog <gagikpog@gmail.com>
2021-09-19 12:37:37 +0500

    refactor

--- script.js
+++ script.js
@@ -1,86 +1,78 @@
-var map1 =[],map2 =[];
-var W = 54,H = 7;
-var activemap1 = true;
-var iter = 0;
-var timeout = 100;
+const W = 54;
+const H = 7;
+const map1 = generateMap(W, H);
+const map2 = generateMap(W, H);
 
-function run(TimeOut = 100)
-{
-	timeout = TimeOut;
-	timer();	
+let activeMap1 = true;
+let generation = 0;
+const timeout = 100;
+
+function run() {
+    fillingMap();
+    renderMap();
+    timer();
 }
 
-function renderMap(){
-	var tbl = document.getElementsByClassName("day");
-	var _map1 = activemap1? map1:map2;
-	for(var n = 0; n < tbl.length;n++)
-	{
-		var i = Math.floor(n/7);
-		var j = n%7;
-		if(_map1[j][i])
-			tbl[n].setAttribute("fill","#7bc96f");
-		else tbl[n].setAttribute("fill","#ebedf0");		
-	}
+function renderMap() {
+    const _map1 = activeMap1 ? map1 : map2;
+    const tbl = document.getElementsByClassName("ContributionCalendar-day");
+    [...tbl].forEach((item, index) => {
+        const i = Math.floor(index / 7);
+        const j = index % 7;
+        item.setAttribute("data-level", _map1[j][i] ? '2' : '0');
+    });
 }
 
-function fillingMap()
-{
-	var tbl = document.getElementsByClassName("day");
-	for (n = 1; n < tbl.length;n++)
-	{
-		var i = Math.floor(n/7);
-		var j = n%7;
-		map1[j][i] = tbl[n].attributes.fill.value != "#ebedf0";	
-	}
+function fillingMap() {
+    const tbl = document.getElementsByClassName("ContributionCalendar-day");
+    [...tbl].forEach((item, index) => {
+        const i = Math.floor(index / 7);
+        const j = index % 7;
+        map1[j][i] = Number(item.getAttribute('data-level')) > 0;
+    });
 }
 
-function createmap(_map1){
-	for (var i = 0; i < H; i++){
-	   _map1[i] = [];
-	    for (var j = 0; j < W; j++){
-	        _map1[i][j] = false;
-		}
-	}
+function generateMap(w, h) {
+    return new Array(h).fill(null).map(() => {
+        return new Array(w).fill(false);
+    });
 }
 
-function CountNeighbors(y,x){
-	var _map1 = activemap1? map1:map2;
-	var res = 0;
-	for (var i = - 1; i <= 1; i++)
-	{
-		for (var j = - 1; j <= 1; j++)
-		{
-			if (_map1[(y + i + H) % H][(x + j + W) % W])
-				res++;
-		}
-	}
-	if (_map1[y][x])
-		res--;
-	return res;
+function countNeighbors(y, x) {
+    const _map1 = activeMap1 ? map1 : map2;
+    let res = 0;
+    for (let i = - 1; i <= 1; i++) {
+        for (let j = - 1; j <= 1; j++) {
+            if (_map1[(y + i + H) % H][(x + j + W) % W]) {
+                res++;
+            }
+        }
+    }
+
+    if (_map1[y][x]) {
+        res--;
+    }
+
+    return res;
 }
 
-function nextGeneration()
-{
-	var _map1 = activemap1 ? map1 : map2;
-	var _map2 = activemap1 ? map2 : map1;
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
-			var n =  CountNeighbors(i,j);
-			_map2[i][j] = (n == 3) || ((n == 2) && (_map1[i][j]));
-		}
-	}
-	activemap1 = !activemap1;
-	iter++;	
+function nextGeneration() {
+    const _map1 = activeMap1 ? map1 : map2;
+    const _map2 = activeMap1 ? map2 : map1;
+    for(let i = 0; i < H; i++) {
+        for(let j = 0; j < W; j++) {
+            const n =  countNeighbors(i, j);
+            _map2[i][j] = (n == 3) || ((n == 2) && (_map1[i][j]));
+        }
+    }
+    activeMap1 = !activeMap1;
+    generation++;
 }
 
 function timer() {
-	nextGeneration();	
-	renderMap();
-	setTimeout(timer, timeout);
+    nextGeneration();
+    renderMap();
+    setTimeout(timer, timeout);
 }
 
-createmap(map1);
-createmap(map2);
-fillingMap();
-renderMap();
 run();
